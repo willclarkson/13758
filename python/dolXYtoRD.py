5da8a1f4d7eda8c3867ebdfa508887f2f620ecac
Will Clarkson <wiclarks@umich.edu>
2016-09-06 15:22:41 +0100

    Updated file-finding syntax following dolphot convention

--- python/dolXYtoRD.py
+++ python/dolXYtoRD.py
@@ -16,8 +16,10 @@ from astropy.io import fits
 from astropy import wcs
 
 def xyToRd(filPhot='blah.xym', filWCS='', filOut='', \
-               colX='col1', colY='col2', \
-               dirWCS='', Verbose=True):
+               filInfo='blah.info', \
+               colX='col3', colY='col4', \
+               dirWCS='', Verbose=True, \
+               Debug=True):
 
     """Converts pixels to ra, dec, using the World Coordinate System
     encoded in (usually fits) file filWCS. Returns the name of the output file if successful, a zero-length blank string otherwise.
@@ -30,6 +32,10 @@ def xyToRd(filPhot='blah.xym', filWCS='', filOut='', \
 
     filOut -- Output file name to contain the X, Y, RA, DEC, Mag
 
+    filInfo -- WCS file might not have a filename like filPhot. In
+    this case, use the .info output from dolphot to find the name of
+    the reference file.
+
     dirWCS -- Directory holding the WCS fits file.
 
     colX -- column in the photometry table holding the X pixel coords
@@ -37,6 +43,8 @@ def xyToRd(filPhot='blah.xym', filWCS='', filOut='', \
     colY -- column in the photometry table holding the Y pixel coords
 
     Verbose -- Provide debug messages to the screen?
+
+    Debug -- print info about the files without actually doing anything on the data
 
     --
 
@@ -68,6 +76,12 @@ def xyToRd(filPhot='blah.xym', filWCS='', filOut='', \
     # Construct the WCS filename using our rules
     consWCS = getWcsNameFromPhot(filPhot)
 
+    # If we've supplied an "info" file to get the reference image
+    # name, use that instead of the photometry file name.
+    if len(filInfo) > 0:
+        if os.access(filInfo, os.R_OK):
+            consWCS = getWcsNameFromInfo(filInfo)
+
     # If no WCS file was given, OR the given WCS file is not readable,
     # use the constructed WCS filename to search for the header.
     if len(filWCS) < 1:
@@ -77,23 +91,36 @@ def xyToRd(filPhot='blah.xym', filWCS='', filOut='', \
         if not os.access(pathWCS, os.R_OK):
             pathWCS = '%s/%s' % (dirWCS, consWCS)
 
-    # OK by this point we have our path for the WCS information. If
-    # this isn't found on disk we should gracefully exit
-    if not os.access(pathWCS, os.R_OK):
-        if Verbose:
-            print "dolXYtoRD.xyToRd FATAL - cannot read WCS path %s" % (pathWCS)
-        return ''
-
     # If we got here, then we have found both the photometry file and
     # the file holding the WCS information that we'll need. Construct
     # the output filename too.
     if len(filOut) < 4:  # (I like descriptive filenames)
         filOut = getOutNameFromPhot(filPhot)
 
+    # some lines for debugging
+    if Debug or Verbose:
+        print "DBG: filPhot: ", filPhot, os.access(filPhot, os.R_OK)
+        print "DBG: filInfo: ", filInfo, os.access(filInfo, os.R_OK)
+        print "DBG: pathWCS: ", pathWCS, os.access(pathWCS, os.R_OK)
+        print "DBG: filOut: ", filOut, os.access(filOut, os.R_OK)
+        
+        if Debug:
+            return
+
+    ## Having decided what to call everything, now look for them on
+    ## disk
+
+    # OK by this point we have our path for the WCS information. If
+    # this isn't found on disk we should gracefully exit
+    if not os.access(pathWCS, os.R_OK):
+        if Verbose:
+            print "dolXYtoRD.xyToRd FATAL - cannot read WCS path %s" % (pathWCS)
+        return 
+
 
     # That's the hard part done. Now we do the easy part - read the
     # data, apply the transformation, and write the result.
-    tPhot = Table.read(filPhot)
+    tPhot = Table.read(filPhot, format='ascii')
     
     hdulist = fits.open(pathWCS)
 
@@ -128,9 +155,11 @@ def getWcsNameFromPhot(filPhot='blah.xym', sSrch='.xym', sRep='.fits'):
 
     """Turns the photometry name into the WCS header filename"""
 
-    # Simplest case - replace the extension from photometry with the
-    # extension from fits
-    outStr = filPhot.replace(sSrch, sRep)
+    # NOTE - search and replace doesn't work if the output file has no
+    # extension! Instead do this the long way...
+    sStem = filPhot.split(sSrch)[0]
+    outStr = '%s%s' % (sStem, sRep)
+
     
     return outStr
 
@@ -138,6 +167,36 @@ def getOutNameFromPhot(filPhot='blah.xym', sSrch='.xym', sRep='.ram'):
 
     """Turns the photometry name into the output filename"""
 
-    outStr = filPhot.replace(sSrch, sRep)
+    # NOTE - search and replace doesn't work if the output file has no
+    # extension! Instead do this the long way...
+    sStem = filPhot.split(sSrch)[0]
+    outStr = '%s%s' % (sStem, sRep)
 
     return outStr
+
+def getWcsNameFromInfo(filInfo='blah.info', sSplit='.chip', sRep='_flc.fits'):
+
+    """Gets the wcs file name from the .info file"""
+
+    if not os.access(filInfo, os.R_OK):
+        return 'NON'
+
+    # Judging by the first example, the second line of the .info file
+    # is the reference image.
+
+    ### Change these rules to use a different strategy to find the
+    ### reference image.
+    with open(filInfo, 'r') as rObj:
+        iCount=0
+        for line in rObj:
+            if iCount > 1:
+                break
+            sFil = line.strip()
+            iCount = iCount + 1
+
+    # Apply our rules to turn the first image name into the WCS image
+    # name.
+    fileStem = sFil.split(sSplit)[0]
+    sWCS = '%s%s' % (fileStem, sRep)
+
+    return sWCS
