4a82ed6fc412917e25f90ae96881852845c1ca37
wiclarks@umich.edu <wiclarks@umich.edu>
2017-04-25 15:48:47 -0400

    more updates to distrib routine

--- python/distribArchival.py
+++ python/distribArchival.py
@@ -19,6 +19,18 @@ def findFits(dirTop="", srchString='.fits'):
 
     return lFits
 
+def getFileInfo(filePath=''):
+    
+    if len(filePath) < 1:
+        return
+
+    if not os.access(filePath, os.R_OK):
+        return
+
+    hdr = fits.getheader(filePath, 0)
+
+    print os.path.split(filePath)[-1], hdr['PROPOSID']
+
 def go():
 
     """Wrapper - does the searching"""
@@ -26,3 +38,6 @@ def go():
     LFits = findFits()
 
     print len(LFits)
+
+    for iFil in range(3):
+        getFileInfo(LFits[iFil])
